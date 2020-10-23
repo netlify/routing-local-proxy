f01917ffc898e95da23c013355d3abb0406b3511
Moritz Gunz <moritz.gunz@gmail.com>
2020-10-23 18:47:16 +0200

    Add npm package stub
    ... to be filled by CI with binaries.

---
+++ scripts/set-version.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const targetVersion = process.env.VERSION || process.argv[2];
+if (!targetVersion) {
+  console.error("No target version specified, set either via the VERSION env variable or via CLI parameter");
+  return;
+}
+
+const packageFile = path.resolve(__dirname, "../package.json");
+
+const contents = JSON.parse(fs.readFileSync(packageFile));
+contents.version = targetVersion;
+
+const newContents = JSON.stringify(contents, null, 2);
+fs.writeFileSync(packageFile, newContents);
